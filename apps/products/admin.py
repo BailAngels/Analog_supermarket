084da8c225c66ddabca292f69453d1c6f85490e3
BailAngels <wolf.abmin.14@gmail.com>
2024-02-08 15:02:55 +0600

    hello

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 from apps.products.models import Product, ProductImages
 
 
-# class ProductImageInline(admin.TabularInline):
-#     model = Product.get_images().through
-#     extra = 1
-#
-#
+class ProductImageInline(admin.TabularInline):
+    model = ProductImages
+    extra = 1
+
+
 @admin.register(ProductImages)
 class ProductImagesAdmin(admin.ModelAdmin):
     list_display = ['image']
@@ -18,3 +18,4 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price']
     list_filter = ['category']
     search_fields = ['title']
+    inlines = [ProductImageInline]
--- utils/image_path.py
+++ utils/image_path.py
@@ -1,6 +1,6 @@
 def upload_avatar_for_user(instance, filename):
-    return f"/avatar/{instance.username}/{filename}"
+    return f"avatar/{instance.username}/{filename}"
 
 
 def upload_product(instance, filename):
-    return f"/product/{instance.product.title}/{filename}"
+    return f"product/{instance.product.title}/{filename}"
